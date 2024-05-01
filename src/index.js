39f3881bb8baaa29c12fdc7df9d34ab6816f7a5a
KiruthikaMKS1617 <mksdevdesk@gmail.com>
2024-05-01 12:15:17 +0530

    Add first loaders to handle css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { run } from "./app/app";
 import { ComponentService } from "./app/components.service";
 import { AlertService } from "./app/alerts.service";
+import "./main.css";
 
 const alertService = new AlertService();
 const componentService = new ComponentService();
--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,15 @@ module.exports = {
     filename: "main.js",
     path: path.join(__dirname, "dist"),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader", // processes tradional js based styles and creates style snippet in head tag
+          "css-loader", // processes imports and convert to traditional js
+        ],
+      },
+    ],
+  },
 };
